13d3d660ba0f3dfc1799e08320360c91b1d0ea66
Sohaib Azhar <sohaibazhar04@gmail.com>
2025-08-22 15:14:41 +0500

    usersServices methoods injected in the users controller.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
@@ -10,29 +11,31 @@ export class UsersController {
     DELETE /users/:id
     */
 
+    constructor(private readonly usersService: UsersService) {}
+
     @Get()  // /users or users?role=value
     findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN' ) {
-        return []
+        return this.usersService.findAll(role)
     }
 
     @Get(':id')   // /users/:id
     findOne(@Param('id') id:string ){
-        return {id}
+        return this.usersService.findOne(+id)   // used unary sign to covnert string to number as expected by findOne function
     }
 
     @Post() // /users
-    create(@Body() user: {} ){
-        return user
+    create(@Body() user: {name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN'} ){
+        return this.usersService.create(user)
     }
 
     @Patch(':id')   // /users/:id
-    update(@Param('id') id:string , @Body() userUpdate : {} ){
-        return {id, ...userUpdate}
+    update(@Param('id') id:string , @Body() userUpdate : {name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN'} ){
+        return this.usersService.update(+id , userUpdate)
     }
 
     @Delete(':id')   // /users/:id
     delete(@Param('id') id:string ){
-        return {id}
+        return this.usersService.delete(+id)
     }
 
     
