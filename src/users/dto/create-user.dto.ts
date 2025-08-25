9f444ffff8fb17c00e58440ebfce72b63084d81a
Sohaib Azhar <sohaibazhar04@gmail.com>
2025-08-25 14:30:10 +0500

    applied class-validator decorators to the create and update DTOs.

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,19 @@
+import { IsString, IsEmail, IsEnum, IsNotEmpty } from 'class-validator';
+
+enum UserRole {
+    INTERN = "INTERN",
+    ENGINEER = "ENGINEER",
+    ADMIN = "ADMIN"
+}
+
 export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @IsEnum(UserRole, { message: "Valid Role Required" })
     role: "INTERN" | "ENGINEER" | "ADMIN";
 }
